e048659c1254216c2112d019bbaefd951f7de331
Roddi Walker <rbwalker@ermpower.com.au>
2018-02-21 13:08:32 +1100

    Added log4net error logging

--- MathServices/MathServices/Program.cs
+++ MathServices/MathServices/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace MathServices
+﻿using log4net;
+using log4net.Config;
+using System;
+
+namespace MathServices
 {
     class Program
     {
         static void Main(string[] args)
         {
+            XmlConfigurator.Configure();
+            ILog logger = LogManager.GetLogger("MathServices");
+
+            try
+            {
+                logger.Info("Service started");
+
+                Console.WriteLine("Service started. Press ENTER to exit.");
+                Console.Read();
+
+                logger.Info("Service stopped");
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Unhandled exception", ex);
+            }
         }
     }
 }
