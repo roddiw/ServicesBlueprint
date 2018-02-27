3ee710db9d043cc337256283ae7021c1cccb54f3
Roddi Walker <rbwalker@ermpower.com.au>
2018-02-27 12:56:23 +1100

    To show how an application service can call a fine-grained backend service, AppServices.AppService.DoubleInt now calls MathServices.IntService.AddInts

--- AppServices/AppServices/Processing/DoubleIntAction.cs
+++ AppServices/AppServices/Processing/DoubleIntAction.cs
@@ -1,5 +1,8 @@
 ﻿using AppServices.Messages;
 using Common.RequestProcessing;
+using Common.RequestProcessing.Messages;
+using MathServices.Messages;
+using System.Linq;
 
 namespace AppServices.Processing
 {
@@ -7,7 +10,28 @@ namespace AppServices.Processing
     {
         public DoubleIntResponse Execute(DoubleIntRequest request)
         {
-            return new DoubleIntResponse(request.Number + request.Number);
+            AddIntsResponse addIntsResponse;
+            var client = new IntService.IntServiceClient();
+            try
+            {
+                // we can use AddIntsRequest constructor because we referenced MathServices.Messages project
+                addIntsResponse = client.AddInts(new AddIntsRequest(request.RequestID, request.RequestingSystem, request.RequestingSystem, request.Number, request.Number));
+                client.Close();
+            }
+            catch
+            {
+                client.Abort();
+                throw;
+            }
+
+            if (addIntsResponse.Errors.Any())
+            {
+                // normally IntService.AddInts errors would not be returned directly as IntService is not visible to the DoubleService client.
+                // instead errors meaningful to the DoubleService.DoubleInt client should be returned.
+                return new DoubleIntResponse(new ResponseError(ErrorCode.ExecutionError, "", "Could not perform operation").ToList());
+            }
+
+            return new DoubleIntResponse(addIntsResponse.Result);
         }
     }
 }
