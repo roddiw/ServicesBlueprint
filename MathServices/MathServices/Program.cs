93d033087f50ca2711f85b2e1d367c0d13585f5d
Roddi Walker <rbwalker@ermpower.com.au>
2018-02-27 11:42:38 +1100

    Tidy up to last checkin

--- MathServices/MathServices/Program.cs
+++ MathServices/MathServices/Program.cs
@@ -1,7 +1,4 @@
-﻿using log4net;
-using log4net.Config;
-using System;
-using System.ServiceModel;
+﻿using System;
 using System.ServiceProcess;
 
 namespace MathServices
@@ -25,55 +22,6 @@ namespace MathServices
                 Console.WriteLine("Attempting to stop service");
                 serviceHostController.StopService();
                 Console.WriteLine("Successfully stopped service");
-            }
-        }
-
-        static void Main2(string[] args)
-        {
-            XmlConfigurator.Configure();
-            ILog logger = LogManager.GetLogger("MathServices");
-
-            try
-            {
-                //
-                // start service
-                //
-
-                logger.Info("Attempting to start service");
-
-                logger.Info("Attempting to initialize resolver");
-                Resolver.Initialize(logger);
-                logger.Info("Successfully initialized resolver");
-
-                logger.Info("Attempting to open service host");
-                var serviceHost = new ServiceHost(typeof(IntService));
-                serviceHost.Open();
-                logger.Info("Successfully opened service host");
-
-                logger.Info("Successfully started service");
-
-                Console.WriteLine("Press ENTER to exit.");
-                Console.Read();
-
-                //
-                // stop service
-                //
-
-                logger.Info("Attempting to stop service");
-
-                logger.Info("Attempting to close service host");
-                serviceHost.Close();
-                logger.Info("Successfully closed service host");
-
-                logger.Info("Attempting to dispose resolver");
-                Resolver.Dispose();
-                logger.Info("Successfully disposed resolver");
-
-                logger.Info("Successfully stopped service");
-            }
-            catch (Exception ex)
-            {
-                logger.Error("Unhandled exception", ex);
             }
         }
     }
